08f248909317f4294be9db98366a2f358f6245f9
HanJing1027 <71305380+HanJing1027@users.noreply.github.com>
2025-03-16 15:49:56 +0800

    前端 Vue 第一次上傳

---
+++ frontend/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/styles/main.scss'
+import 'boxicons/css/boxicons.min.css'
+
+createApp(App).mount('#app')
---
+++ frontend/src/views/KaguyaShinomiya.vue
@@ -0,0 +1,5 @@
+<template></template>
+
+<script setup></script>
+
+<style src="../assets/styles/views/kaguya-shinomiya.scss" scoped></style>
