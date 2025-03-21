5fe07ade582641f0027ba423fa3818a4d768b1fc
SimonGit <simon88.wang@gmail.com>
2025-03-22 01:30:30 +0800

    feat&fix：新增api與整理route
    1.新增產品api
    2.整理route路徑
    3.新增npm快捷鍵

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\product;
+use App\Models\products_image;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +17,97 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+
+// 指定商品的資料
+Route::get('/product-json/{id}', function ($id) {
+  $product = product::find($id);
+
+  if (!$product) {
+    return response()->json(['error' => 'Product not found'], 404);
+  }
+
+  $images = products_image::where('product_id', $id)->get();
+  // 將字串形式的陣列轉換為實際PHP陣列
+  $colors = json_decode(str_replace("'", '"', $product->colors), true);
+  $size = json_decode(str_replace("'", '"', trim($product->size)), true);
+
+  $result = [
+    'product' => [
+      'id' => $product->id,
+      'name' => $product->name,
+      'colors' => $colors,
+      'size' => $size,
+      'description' => $product->description,
+      'price' => $product->price,
+      'images' => $images->map(function ($image) {
+        return [
+          'id' => $image->id,
+          'src' => $image->src,
+          'isMain' => $image->isMain,
+          'product_id' => $image->product_id,
+        ];
+      }),
+    ],
+  ];
+
+  return response()->json($result);
+});
+
+// 所有商品的資料
+Route::get('/products-json/all', function () {
+  $products = product::all();
+
+  $result = $products->map(function ($product) {
+    $images = products_image::where('product_id', $product->id)->get();
+
+    // 將字串形式的陣列轉換為實際PHP陣列
+    $colors = json_decode(str_replace("'", '"', $product->colors), true);
+    $size = json_decode(str_replace("'", '"', trim($product->size)), true);
+
+    return [
+      'id' => $product->id,
+      'name' => $product->name,
+      'colors' => $colors,
+      'size' => $size,
+      'description' => $product->description,
+      'price' => $product->price,
+      'images' => $images->map(function ($image) {
+        return [
+          'id' => $image->id,
+          'src' => $image->src,
+          'isMain' => $image->isMain,
+          // 'product_id' => $image->product_id,
+        ];
+      }),
+    ];
+  });
+
+  return response()->json(['products' => $result]);
+});
+
+//新增商品
+Route::post('/product/add', function (Request $request) {
+  try {
+    $product = new product();
+    $product->name = $request->name;
+    $product->colors = json_encode($request->colors);
+    $product->size = json_encode($request->size);
+    $product->description = $request->description;
+    $product->price = $request->price;
+    $product->save();
+
+    $images = $request->images;
+    foreach ($images as $image) {
+      $product_image = new products_image();
+      $product_image->src = $image['src'];
+      $product_image->isMain = $image['isMain'];
+      $product_image->product_id = $product->id;
+      $product_image->save();
+    }
+  } catch (\Exception $e) {
+    return response('Product added failed', 500);
+  }
+  return response('Product added successed', 200);
 });
