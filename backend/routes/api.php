82e65a0469a1ccd5612136dc7ff9a0699834e8a7
SimonGit <simon88.wang@gmail.com>
2025-03-30 23:35:47 +0800

    feat：新增更新商品api
    1.更新商品api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Models\product;
 use App\Models\products_image;
 use Illuminate\Support\Facades\Validator;
-
 use function Laravel\Prompts\error;
+
+//設定時區
+date_default_timezone_set('Asia/Taipei');
+
 
 /*
 |--------------------------------------------------------------------------
@@ -62,7 +65,6 @@ Route::get('/product-json/{id}', function ($id) {
 
 // 所有商品的資料
 Route::get('/products-json/all', function () {
-
 
   $products = product::all();
 
@@ -131,70 +133,6 @@ Route::get('/product/search', function (Request $request) {
 #endregion
 
 #region 新增商品
-//新增商品
-Route::post('/product/added', function (Request $request) {
-  error_log("request data:" . $request);
-  try {
-    //確認資料是否為空
-    $validator = Validator::make($request->all(), [
-      'name' => 'required',
-      'colors' => 'required|array',
-      'size' => 'required|array',
-      'description' => 'required',
-      'price' => 'required|numeric',
-      'images' => 'required|array',
-      'images.*.src' => 'required',
-      'images.*.isMain' => 'required|boolean',
-    ]);
-
-    if ($validator->fails()) {
-      return response($validator->errors(), 400);
-    }
-    $product = new product();
-    $product->name = $request->name;
-    $product->colors = json_encode($request->colors);
-    $product->size = json_encode($request->size);
-    $product->description = $request->description;
-    $product->price = $request->price;
-    $product->save();
-
-    $images = $request->images;
-    foreach ($images as $image) {
-      $product_image = new products_image();
-      $product_image->src = $image['src'];
-      $product_image->isMain = $image['isMain'];
-      $product_image->product_id = $product->id;
-      $product_image->save();
-    }
-  } catch (\Exception $e) {
-    return response('Product added failed:' . $e, 500);
-  }
-  return response('Product added successed', 200);
-});
-
-
-
-//儲存圖片至本地
-Route::post('/product/image/upload', function (Request $request) {
-  error_log("request data:" . $request);
-  $image = $request->file('image');
-  $imageName = time() . '.' . $image->extension();
-  $image->move(public_path('images'), $imageName);
-  return response()->json(['image' => $imageName]);
-});
-
-//儲存多個圖片
-Route::post('/product/images/upload', function (Request $request) {
-  error_log("request data:" . $request);
-  $images = $request->file('images');
-  $imageNames = [];
-  foreach ($images as $image) {
-    $imageName = time() . '.' . $image->extension();
-    $image->move(public_path('images'), $imageName);
-    array_push($imageNames, $imageName);
-  }
-  return response()->json(['images' => $imageNames]);
-});
 
 //儲存產品與多個圖片
 Route::post('/product/add', function (Request $request) {
@@ -202,8 +140,8 @@ Route::post('/product/add', function (Request $request) {
   // 驗證請求資料
   $validator = Validator::make($request->all(), [
     'jsonData' => 'required|json',
-    'mainImage' => 'required|file|mimes:jpeg,png,jpg,gif|max:2048', // 主圖片驗證
-    'images.*' => 'file|mimes:jpeg,png,jpg,gif|max:2048', // 副圖片驗證
+    'mainImage' => 'required|file|mimes:jpeg,png,jpg,gif', // 主圖片驗證
+    'images.*' => 'file|mimes:jpeg,png,jpg,gif', // 副圖片驗證
   ]);
 
   if ($validator->fails()) {
@@ -224,7 +162,7 @@ Route::post('/product/add', function (Request $request) {
     //主圖片
     $imageNames = [];
     $mainImage = $request->file('mainImage');
-    $mainImageName = time() . '_main.' . $mainImage->extension();
+    $mainImageName = date('YmdHis') . '_main.' . $mainImage->extension();
     $mainImage->move(public_path('images'), $mainImageName);
     array_push($imageNames, $mainImageName);
 
@@ -232,7 +170,7 @@ Route::post('/product/add', function (Request $request) {
     $otherImages = $request->file('images');
     if ($otherImages) {
       foreach ($otherImages as $index => $image) {
-        $imageName = time() . "_other_{$index}." . $image->extension();
+        $imageName = date('YmdHis') . "_{$index}." . $image->extension();
         $image->move(public_path('images'), $imageName);
         array_push($imageNames, $imageName);
       }
@@ -249,6 +187,102 @@ Route::post('/product/add', function (Request $request) {
     return response('Product added successed', 200);
   } catch (\Exception $e) {
     return response('Product added failed:' . $e, 500);
+  }
+});
+
+//儲存圖片至本地
+Route::post('/product/image/testUpload', function (Request $request) {
+  error_log("request data:" . $request);
+  $image = $request->file('image');
+  $imageName = date('YmdHis') . 'test.' . $image->extension();
+  $image->move(public_path('images'), $imageName);
+  return response()->json(['image' => $imageName]);
+});
+
+//儲存多個圖片
+Route::post('/product/images/testUpload', function (Request $request) {
+  error_log("request data:" . $request);
+  $images = $request->file('images');
+  $imageNames = [];
+  foreach ($images as $image) {
+    $imageName = date('YmdHis') . 'test.' . $image->extension();
+    $image->move(public_path('images'), $imageName);
+    array_push($imageNames, $imageName);
+  }
+  return response()->json(['images' => $imageNames]);
+});
+
+#endregion
+
+#region 更新商品
+//更新商品
+Route::post('/product/update', function (Request $request) {
+  // 驗證請求資料
+  $validator = Validator::make($request->all(), [
+    'jsonData' => 'required|json',
+    'mainImage' => 'required|file|mimes:jpeg,png,jpg,gif,webp', // 主圖片驗證
+    'images.*' => 'file|mimes:jpeg,png,jpg,gif,webp', // 副圖片驗證
+  ]);
+
+  if ($validator->fails()) {
+    return response($validator->errors(), 400);
+  }
+  try {
+    // 解析 JSON 資料
+    $jsonData = json_decode($request->input('jsonData'), true);
+    //商品資料
+    $product = product::find($jsonData['id']);
+    if (!$product) {
+      return response('Product not found', 404);
+    }
+    $product->name = $jsonData['name'];
+    $product->colors = json_encode($jsonData['colors']);
+    $product->size = json_encode($jsonData['size']);
+    $product->description = $jsonData['description'];
+    $product->price = $jsonData['price'];
+    $product->save();
+
+    // 刪除舊的圖片與資料庫資料
+    $oldImages = products_image::where('product_id', $product->id)->get();
+    foreach ($oldImages as $oldImage) {
+      $oldImagePath = public_path('images/' . $oldImage->src);
+      if (file_exists($oldImagePath)) {
+        unlink($oldImagePath); // 刪除舊圖片
+      }
+      $oldImage->delete(); // 刪除資料庫記錄
+    }
+
+    $imageNames = [];
+
+    // 處理主圖片
+    $mainImage = $request->file('mainImage');
+    $mainImageName = date('YmdHis') . '_main.' . $mainImage->extension();
+    $mainImage->move(public_path('images'), $mainImageName);
+    array_push($imageNames, $mainImageName);
+
+    // 副圖片
+    if ($request->hasFile('images')) {
+      $otherImages = $request->file('images');
+      if ($otherImages) {
+        foreach ($otherImages as $index => $image) {
+          $imageName = date('YmdHis') . "_{$index}." . $image->extension();
+          $image->move(public_path('images'), $imageName);
+          array_push($imageNames, $imageName);
+        }
+      }
+    }
+
+    //儲存圖片資料
+    foreach ($imageNames as $imageName) {
+      $product_image = new products_image();
+      $product_image->src = $imageName;
+      $product_image->isMain = $imageName == $mainImageName ? 1 : 0;
+      $product_image->product_id = $product->id;
+      $product_image->save();
+    }
+    return response('Product updated successed', 200);
+  } catch (\Exception $e) {
+    return response('Product updated failed:' . $e, 500);
   }
 });
 #endregion
